b585f22020ee4e0aa1ad96dc3061208dc7ca9310
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-11-03 19:03:27 +0200

    Refactoring a files structure

--- main.py
+++ main.py
@@ -4,38 +4,27 @@ import os
 import sys
 
 from aiogram import Bot, Dispatcher
-from aiogram import html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
 from dotenv import load_dotenv
+
+from app.handlers import router
 
 load_dotenv()
 
-# Bot token can be obtained via https://t.me/BotFather
-token = os.getenv("BOT_TOKEN")
-
-# All handlers should be attached to the Router (or Dispatcher)
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
 
 async def main() -> None:
+    # Bot token can be obtained via https://t.me/BotFather
+    token = os.getenv("BOT_TOKEN")
+
+    # All handlers should be attached to the Router (or Dispatcher)
+    dp = Dispatcher()
+
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # Router
+    dp.include_router(router=router)
 
     # And the run events dispatching
     await dp.start_polling(bot)
