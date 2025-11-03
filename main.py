f7702d65fa7a52bcb913e867449c9b33c554feba
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-11-03 18:46:54 +0200

    Add simple bot with command_start_handler

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
+import asyncio
+import logging
 import os
+import sys
 
+from aiogram import Bot, Dispatcher
+from aiogram import html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Bot token can be obtained via https://t.me/BotFather
-TOKEN = os.getenv("BOT_TOKEN")
+token = os.getenv("BOT_TOKEN")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
-    pass
+    # Display info about Bot state
+    if os.getenv("IS_DEBUG"):
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Program interrupted by user. Exiting gracefully.")
