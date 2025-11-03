3f4eae956e943996f2d5eab30e7f25c04627510f
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-11-03 18:03:33 +0200

    Init project

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = os.getenv("BOT_TOKEN")
+
+if __name__ == "__main__":
+    pass
