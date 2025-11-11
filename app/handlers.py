0c25777f2f662bb54cd561b1ae89b11ab0e19e3a
Olena Kysilova <e.a.kiseljova@gmail.com>
2025-11-11 19:39:30 +0200

    Customize info message with weather data

--- app/handlers.py
+++ app/handlers.py
@@ -19,17 +19,42 @@ async def command_start_handler(message: Message) -> None:
     # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    text = (
+        f"Hello, {html.bold(message.from_user.full_name)}!\n\n"
+        f"🗺 Send the location where you want to know the weather. "
+        f"Or click the Register button 👇 to start register process. "
+        f"Once registered, you'll be able to save your favorite locations and quickly check the weather there ☺️"
+    )
+
     await message.answer(
-        text=f"Hello, {html.bold(message.from_user.full_name)}!\n\n"
-             f"🗺 Send the location where you want to know the weather. "
-             f"Or click the Register button 👇 to start register process. "
-             f"Once registered, you'll be able to save your favorite locations and quickly check the weather there ☺️",
+        text=text,
         reply_markup=kb.main,
     )
 
 
 @router.message(F.location)
 async def location_handler(message: Message) -> None:
-    weather = get_weather(lat=message.location.latitude, lon=message.location.longitude)
+    data = get_weather(lat=message.location.latitude, lon=message.location.longitude)
+    weather = data["weather"][0]
+    main = data["main"]
+    wind = data["wind"]
 
-    await message.reply(text=html.code(weather))
+    description = f"_Weather:_ *{weather["description"].capitalize()}*"
+    temperature = f"_Temperature:_ *{main["temp"]} °C*"
+    feels_like = f"_Feels like:_ *{main["feels_like"]} °C*"
+    pressure = f"_Pressure:_ *{main["pressure"]} hPa*"
+    humidity = f"_Humidity:_ *{main["humidity"]} %*"
+    wind_speed = f"_Wind:_ *{wind["speed"]} m/s*"
+
+    text = "\n\n".join(
+        [description, temperature, feels_like, pressure, humidity, wind_speed]
+    )
+
+    icon = "https://openweathermap.org/img/wn/{0}@2x.png"
+
+    await message.reply_photo(
+        photo=icon.format(weather["icon"]),
+        caption=text,
+        parse_mode="Markdown",
+        reply_markup=kb.main,
+    )
